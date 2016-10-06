378a86be9beafba2b54fc604a921de8c06531a6e
TheDropOne <TheDropOne@gmail.com>
2016-10-06 13:52:35 +0300

    Added Matrix, Reader classes

--- src/by/thedrop/entity/Runner.java
+++ src/by/thedrop/entity/Runner.java
@@ -1,8 +1,13 @@
 package by.thedrop.entity;
 
+import java.io.File;
+
 /**
  * Created by Kuryakov on 05-Oct-16.
  */
 public class Runner {
-    
+    public static void main(String[] args) throws Exception {
+        Matrix matrix = new Matrix(new File("input.txt"));
+        matrix.print();
+    }
 }
